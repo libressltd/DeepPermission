39feb5c7f7b233b469750612a263997b7a332a4d
iConcept <iconcept@MacBook-Pro.local>
2016-11-14 17:41:25 +0700

    Add export function

--- src/controllers/SettingController.php
+++ src/controllers/SettingController.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\Models\Permission_group;
 use App\Models\Permission;
+use App\Models\Role;
+use App\Models\Role_permission;
+use Excel;
 
 class SettingController extends Controller
 {
@@ -86,37 +89,60 @@ class SettingController extends Controller
     {
         //
     }
-	
-	public function getInitial()
-	{
-		Permission_group::addIfNotExist("Permission", "permission");
-		Permission_group::addIfNotExist("Permission Group", "permission_group");
-		Permission_group::addIfNotExist("Role", "role");
-		Permission_group::addIfNotExist("User Role", "user_role");
-		
-		Permission::addIfNotExist("Add", "permission.add");
-		Permission::addIfNotExist("Edit", "permission.edit");
-		Permission::addIfNotExist("Delete", "permission.delete");
-		Permission::addIfNotExist("View", "permission.view");
-		
-		Permission::addIfNotExist("Add", "permission_group.add");
-		Permission::addIfNotExist("Edit", "permission_group.edit");
-		Permission::addIfNotExist("Delete", "permission_group.delete");
-		Permission::addIfNotExist("View", "permission_group.view");
-		
-		Permission::addIfNotExist("Add", "role.add");
-		Permission::addIfNotExist("Edit", "role.edit");
-		Permission::addIfNotExist("Delete", "role.delete");
-		Permission::addIfNotExist("View", "role.view");
-		
-		Permission::addIfNotExist("View", "user_role.view");
-		Permission::addIfNotExist("Setting", "permission.setting");
-		
-		return redirect(url("permission/setting"))->with('dp_announce', 'Database initial success');
-	}
+    
+    public function getInitial()
+    {
+        Permission_group::addIfNotExist("Permission", "permission");
+        Permission_group::addIfNotExist("Permission Group", "permission_group");
+        Permission_group::addIfNotExist("Role", "role");
+        Permission_group::addIfNotExist("User Role", "user_role");
+        
+        Permission::addIfNotExist("Add", "permission.add");
+        Permission::addIfNotExist("Edit", "permission.edit");
+        Permission::addIfNotExist("Delete", "permission.delete");
+        Permission::addIfNotExist("View", "permission.view");
+        
+        Permission::addIfNotExist("Add", "permission_group.add");
+        Permission::addIfNotExist("Edit", "permission_group.edit");
+        Permission::addIfNotExist("Delete", "permission_group.delete");
+        Permission::addIfNotExist("View", "permission_group.view");
+        
+        Permission::addIfNotExist("Add", "role.add");
+        Permission::addIfNotExist("Edit", "role.edit");
+        Permission::addIfNotExist("Delete", "role.delete");
+        Permission::addIfNotExist("View", "role.view");
+        
+        Permission::addIfNotExist("View", "user_role.view");
+        Permission::addIfNotExist("Setting", "permission.setting");
+        
+        return redirect(url("permission/setting"))->with('dp_announce', 'Database initial success');
+    }
     
     function getExport()
     {
+        Excel::create('DeepPermissionExport', function($excel) {
+            $excel->sheet('Permission_group', function($sheet) {
+                $sheet->fromModel(Permission_group::get(), null, "A1", false, false);
+            });
+            $excel->sheet('Permission', function($sheet) {
+                $sheet->fromModel(Permission::get(), null, "A1", false, false);
+            });
+            $excel->sheet('Role', function($sheet) {
+                $sheet->fromModel(Role::get(), null, "A1", false, false);
+            });
+            $excel->sheet('Role_permission', function($sheet) {
+                $sheet->fromModel(Role_permission::get(), null, "A1", false, false);
+            });
+        })->export('xls');
+    }
 
+    function postImport($request)
+    {
+        $file = Input::file('import');
+        $filename = $this->doSomethingLikeUpload($file);
+
+        return Excel::load($filename, function($reader) {
+
+        })->get();
     }
 }
