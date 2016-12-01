02dc20b7b8f3926ba9cdf415533b64b0ef145aa0
iConcept <iconcept@MacBook-Pro.local>
2016-12-01 09:45:59 +0700

    Fix problem of pagination and icheck

--- src/views/app.blade.php
+++ src/views/app.blade.php
@@ -8,6 +8,12 @@
 			checkboxClass: 'icheckbox_minimal-blue',
 			radioClass: 'iradio_minimal-blue'
 		});
+		$('.data-table').on('draw.dt', function () {
+		    $('input[type="checkbox"], input[type="radio"]').iCheck({
+				checkboxClass: 'icheckbox_minimal-blue',
+				radioClass: 'iradio_minimal-blue'
+			});
+		});
 	});
 </script>
 
