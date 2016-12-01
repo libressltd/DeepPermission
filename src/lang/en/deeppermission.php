359708cfe4c562442f36487ef998d14ad537e3a3
iConcept <iconcept@MacBook-Pro.local>
2016-12-01 20:44:03 +0700

    Add lang file

---
+++ src/lang/en/deeppermission.php
@@ -0,0 +1,122 @@
+<?php
+
+return array (
+  'alert' => 
+  array (
+    'permission' => 
+    array (
+      'created' => 'Permission is created',
+      'updated' => 'Permission is updated',
+      'deleted' => 'Permission is deleted',
+    ),
+    'group' => 
+    array (
+      'created' => 'Permission\'s group is created',
+      'updated' => 'Permission\'s group is updated',
+      'deleted' => 'Permission\'s group is deleted',
+    ),
+    'role' => 
+    array (
+      'created' => 'Role is created',
+      'updated' => 'Role is updated',
+      'deleted' => 'Role is deleted',
+    ),
+    'role_permission' => 
+    array (
+      'updated' => 'Role\'s permissions are updated',
+    ),
+    'setting' => 
+    array (
+      'initial' => 'Initial data is inserted',
+    ),
+    'user_permission' => 
+    array (
+      'updated' => 'User\'s permissions are updated',
+    ),
+    'user_role' => 
+    array (
+      'updated' => 'User\'s roles are updated',
+    ),
+  ),
+  'header' => 
+  array (
+    'title' => 'Role & permission management',
+  ),
+  'sidebox' => 
+  array (
+    'user_role' => 'User\'s role',
+    'role' => 'Role',
+    'group' => 'Permission group',
+    'permission' => 'Permission',
+    'setting' => 'Setting',
+  ),
+  'permission' => 
+  array (
+    'add' => 'Add new Permission',
+    'edit' => 'Edit Permission',
+    'name' => 
+    array (
+      'hint' => 'Permission\'s name',
+    ),
+    'code' => 
+    array (
+      'hint' => 'Permission\'s code',
+      'note' => 'For developer only',
+    ),
+    'group_permission' => 'Permission Group',
+    'title' => 'Permission management',
+  ),
+  'general' => 
+  array (
+    'action' => 'Action',
+    'are_you_sure' => 'Are you sure?',
+    'quickadd' => 'Quick Add',
+  ),
+  'group' => 
+  array (
+    'add' => 'Add new Permission Group',
+    'edit' => 'Edit Permission Group',
+    'name' => 
+    array (
+      'hint' => 'Permission Group\'s name',
+    ),
+    'code' => 
+    array (
+      'hint' => 'Permission Group\'s code',
+      'note' => 'For developer only',
+    ),
+    'title' => 'Permission Group management',
+    'permission' => 'Permission',
+  ),
+  'role' => 
+  array (
+    'add' => 'Add new Role',
+    'edit' => 'Edit Role',
+    'name' => 
+    array (
+      'hint' => 'Role\'s name',
+    ),
+    'code' => 
+    array (
+      'hint' => 'Role\'s code',
+      'note' => 'For developer only',
+    ),
+    'title' => 'Role management',
+    'permission_of' => '\'s Roles',
+  ),
+  'setting' => 
+  array (
+    'title' => 'Setting',
+    'initial_text' => 'Initial: Insert all initial Permission and Permission Group',
+    'initial' => 'Initial',
+    'export' => 'Export',
+    'import' => 'Import',
+  ),
+  'user' => 
+  array (
+    'username' => 'Username',
+    'email' => 'Email',
+    'permission_of' => '\'s Permission',
+    'role' => 'User\'s Role',
+  ),
+);
