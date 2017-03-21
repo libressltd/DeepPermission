0e81fe09f664256baafdc3e9072c744b83a2fae0
iConcept <iconcept@MacBook-Pro.local>
2017-03-21 13:36:13 +0700

    Fix bug

--- src/views/export/role_permission.blade.php
+++ src/views/export/role_permission.blade.php
@@ -1,12 +1,12 @@
 <html>
-	<tr>
+    <tr>
     <td>role</td>
     <td>permission</td>
     </tr>
-	@foreach (App\Models\Role_permission::with("role", "permission")->get() as $rp)
-	<tr>
-    <td>{{ $rp->role->code }}</td>
-    <td>{{ $rp->permission->code }}</td>
+    @foreach (App\Models\Role_permission::with("role", "permission")->get() as $rp)
+    <tr>
+    <td>{{ @$rp->role->code }}</td>
+    <td>{{ @$rp->permission->code }}</td>
     </tr>
     @endforeach
 </html>
